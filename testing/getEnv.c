f698fe0865413f34302e8f1a17f3676180410f4b
Your Name <you@example.com>
2024-02-26 16:26:48 +0200

    getEnv.c

---
+++ testing/getEnv.c
@@ -0,0 +1,33 @@
+#include "shell.h"
+#include <string.h>
+
+char *getEnv(char *envVar)
+{
+	int i;
+	char *tmp, *key, *valueOfKey, *env;
+
+	key = tmp = valueOfKey = NULL;
+
+	for (i = 0; environ[i]; i++)
+	{
+		tmp = strdup(environ[i]);
+		key = strtok(tmp, "=");
+		if (strcmp(key, envVar) == 0)
+		{
+			valueOfKey = strtok(NULL, "\n");
+			env = strdup(valueOfKey);
+			free(tmp);
+			return (env);
+		}
+		free(tmp), tmp = NULL;
+	}
+
+	return (NULL);
+}
+
+int main(void)
+{
+	char *env = getEnv("PWD");
+	printf("%s",env );
+		free(env), env = NULL;
+}
